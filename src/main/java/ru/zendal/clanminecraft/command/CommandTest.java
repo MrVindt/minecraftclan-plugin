8e739934fa4b9e732f6b3ac3154cfc5f2ad4d2c1
MrVindt <ivan.vindt15@gmail.com>
2020-01-31 11:40:55 +0300

    Update CommandTest.java

--- src/main/java/ru/zendal/clanminecraft/command/CommandTest.java
+++ src/main/java/ru/zendal/clanminecraft/command/CommandTest.java
@@ -1,12 +1,20 @@
 package ru.zendal.clanminecraft.command;
 
+import com.google.inject.Inject;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class CommandTest implements CommandExecutor {
+
+    @Inject
+    public CommandTest(JavaPlugin jP){
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         return false;
     }
+
 }
