28f146bcd22f755749be6ecf0f50136d649f9143
MrVindt <ivan.vindt15@gmail.com>
2020-02-05 20:55:11 +0300

    add class Event

---
+++ src/main/java/ru/zendal/clanminecraft/event/Event.java
@@ -0,0 +1,12 @@
+package ru.zendal.clanminecraft.event;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class Event implements Listener {
+    @EventHandler
+    public void onBreakBlock(BlockBreakEvent blockBreakEvent){
+
+    }
+}
