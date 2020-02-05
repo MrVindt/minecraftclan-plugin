086bbedfee9b0d8451f3f3e6701dcfbe14a8427a
MrVindt <ivan.vindt15@gmail.com>
2020-02-05 22:45:03 +0300

    add method getClanByChunk

--- src/main/java/ru/zendal/clanminecraft/event/Event.java
+++ src/main/java/ru/zendal/clanminecraft/event/Event.java
@@ -1,12 +1,30 @@
 package ru.zendal.clanminecraft.event;
 
+import com.google.inject.Inject;
+import org.bukkit.Chunk;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import ru.zendal.clanminecraft.сlan.ClanManager;
 
 public class Event implements Listener {
+
+    public final ClanManager clanManager;
+
+    @Inject
+    public Event(ClanManager clanManager) {
+        this.clanManager = clanManager;
+    }
+
     @EventHandler
     public void onBreakBlock(BlockBreakEvent blockBreakEvent){
+        if (getClanByChunk(blockBreakEvent.getPlayer()))
+            blockBreakEvent.setCancelled(true);
+    }
 
+    boolean getClanByChunk(Player player){
+        var chunkNow = player.getLocation().getChunk();
+        return clanManager.getAllClans().stream().anyMatch(clan -> clan.getMainChunk() == chunkNow && clan.getMemberList().equals(player.getName()));
     }
 }
