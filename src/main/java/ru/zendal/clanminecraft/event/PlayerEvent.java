8b5ca3488c040fce954e388949ba7515970a6fb1
Alexander Chapchuk <gasfull98@gmail.com>
2020-02-10 16:21:23 +0300

    Refactor code.

--- src/main/java/ru/zendal/clanminecraft/event/PlayerEvent.java
+++ src/main/java/ru/zendal/clanminecraft/event/PlayerEvent.java
@@ -1,11 +1,11 @@
 package ru.zendal.clanminecraft.event;
 
 import com.google.inject.Inject;
+import org.bukkit.Chunk;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import ru.zendal.clanminecraft.component.i18n.PluginLocalization;
 import ru.zendal.clanminecraft.сlan.ClanManager;
@@ -26,7 +26,7 @@ public class PlayerEvent implements Listener {
 
     @EventHandler
     public void onBreakBlock(BlockBreakEvent blockBreakEvent) {
-        if (!getResultOnEvent(blockBreakEvent.getPlayer(), blockBreakEvent)) {
+        if (!this.checkPermissionForPlayerAtChuck(blockBreakEvent.getPlayer(), blockBreakEvent.getBlock().getChunk())) {
             blockBreakEvent.getPlayer().sendMessage(pluginLocalization.getCommandLocale().getOnClanEventOnBreakBlockError());
             blockBreakEvent.setCancelled(true);
         }
@@ -34,18 +34,37 @@ public class PlayerEvent implements Listener {
 
     @EventHandler
     public void onPutBlock(BlockPlaceEvent blockPlaceEvent) {
-        if (!getResultOnEvent(blockPlaceEvent.getPlayer(), blockPlaceEvent)) {
+        if (!this.checkPermissionForPlayerAtChuck(blockPlaceEvent.getPlayer(), blockPlaceEvent.getBlock().getChunk())) {
             blockPlaceEvent.getPlayer().sendMessage(pluginLocalization.getCommandLocale().getOnClanEventOnPutBlockError());
             blockPlaceEvent.setCancelled(true);
         }
     }
 
-    private boolean getResultOnEvent(Player player, BlockEvent blockEvent) {
+    /**
+     *
+     * @param player
+     * @param chunkAtBlock
+     * @return
+     */
+    private boolean checkPermissionForPlayerAtChuck(Player player, Chunk chunkAtBlock) {
         var chunkNow = player.getLocation().getChunk();
-        var chunkInClan = clanManager.getAllClans().stream().anyMatch(clan -> clan.getMainChunk().getX() == chunkNow.getX() && clan.getMainChunk().getZ() == chunkNow.getZ()
-                || blockEvent.getBlock().getChunk() != chunkNow);
+        var chunkInClan = clanManager.getAllClans().stream().anyMatch(clan ->
+                this.equalsChuck(clan.getMainChunk(), chunkNow) || this.equalsChuck(chunkAtBlock, chunkNow)
+        );
         var playerInClan = clanManager.getAllClans().stream().anyMatch(clan ->
                 clan.getMemberList().stream().anyMatch(member -> member.getPlayer().getUniqueId().equals(player.getUniqueId())));
         return !chunkInClan || playerInClan;
+    }
+
+    /**
+     * Comparing two chuck between self
+     *
+     * @param chunk        first chuck
+     * @param chunkAnother second chuck
+     * @return {@code true} if two chuck equals between self else {@code false}
+     */
+    private boolean equalsChuck(Chunk chunk, Chunk chunkAnother) {
+        return chunk.getX() == chunkAnother.getX() &&
+                chunk.getZ() == chunkAnother.getZ();
     }
 }
